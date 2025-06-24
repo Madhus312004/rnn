b90ab34a97e00e224cccd2131456078dae089cb1
Madhus312004 <madhumithas909@gmail.com>
2025-06-24 21:03:30 +0530

    added py file for streamlit

--- simplernn.py
+++ simplernn.py
@@ -1,7 +1,40 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.datasets import imdb
+from tensorflow.keras.preprocessing import sequence
+from tensorflow.keras.models import load_model
 import streamlit as st
 
-st.title("Hello, Streamlit!")
+word_index=imdb.get_word_index()
+reverse_word_index={value:key for key,value in word_index.items()}
 
-st.write("This is a simple Streamlit app running successfully.")
+model=load_model(r'rnn\simplernn_imdb_new.h5')
 
-st.write('madhu')
+
+def decode_review(encoded_review):
+    return ' '.join([reverse_word_index.get(i-3,'?') for i in encoded_review])
+
+def preprocess_text(text):
+    words=text.lower().split()
+    encoded_review=[word_index.get(word,2)+3 for word in words] #seach in word_index if not present gets value from 2nd index which is reserved to be unknown
+    # +3 since words start from 4 as 0 is for padding, 1 is for start token, 2 is for unknown token
+    padded_review=sequence.pad_sequences([encoded_review],maxlen=500)
+    return padded_review
+
+
+st.title('IMDB Movie Review Sentiment Analysis')
+st.write('Enter a movie review to classify it as positive or negative')
+
+user_input=st.text_area('Movie review')
+
+if st.button('Classify'):
+    preprocessed_input=preprocess_text(user_input)
+
+    prediction=model.predict(preprocessed_input)
+    sentiment='Positive' if prediction[0][0] > 0.5 else 'Negative'
+
+    st.write(f'Sentiment: {sentiment}')
+
+    st.write(f'Prediction score: {prediction[0][0]}')
+else:
+    st.write('Please enter a movie review')
